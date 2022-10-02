0691c3825c0b206fcc9879db397f9e1e467dcc9e
valeriy-brunov <valer1978@bk.ru>
2022-10-02 17:31:41 +0400

    Отформатировал код.

--- webroot/js/components/ext/ajax.js
+++ webroot/js/components/ext/ajax.js
@@ -2,59 +2,58 @@
  * AJAX-запрос.
  */
 export default {
-
-	/**
-	 * Параметры запроса по умолчанию.
-	 */
-	get DEFAULT_PARAMS() {
-		return {
-			url: '',
-			method: 'GET',
-			async: true,
-			success: function(html) {},
-			beforeSend: function() {},
-			error: function(status, statusText) {
-				if (status != 200) {
-					console.log('Ошибка запроса' + status + ':' + statusText);
-				}
-			},
-			errorConnect: function() {
-				console.log('Ошибка соединения!');
-			},
-			progress: function(e) {
-				/*
-				if ( e.lengthComputable ) {
-					alert('Получено ${e.loaded} из ${e.total} байт');
-				}
-				else {
-					alert('Получено ${e.loaded} байт');
-				}
-				*/
-			},
-			complete: function( request ) {
-				// Готово, получили ${request.response.length} байт.
-				// Если ответ в виде JSON: request.response.имя_литерала
-			},
+    /**
+     * Параметры запроса по умолчанию.
+     */
+    get DEFAULT_PARAMS() {
+	return {
+	    url: '',
+	    method: 'GET',
+	    async: true,
+	    success: function(html) {},
+	    beforeSend: function() {},
+	    error: function(status, statusText) {
+		if (status != 200) {
+		    console.log('Ошибка запроса' + status + ':' + statusText);
 		}
-	},
+	    },
+	    errorConnect: function() {
+		console.log('Ошибка соединения!');
+	    },
+	    progress: function(e) {
+		/*
+		if ( e.lengthComputable ) {
+		    alert('Получено ${e.loaded} из ${e.total} байт');
+		}
+		else {
+		    alert('Получено ${e.loaded} байт');
+		}
+		*/
+	    },
+	    complete: function( request ) {
+		// Готово, получили ${request.response.length} байт.
+		// Если ответ в виде JSON: request.response.имя_литерала
+	    },
+	}
+    },
 
-	/**
-	 * XMLHttpRequest-запрос.
-	 * 
-	 * @param {object} param
-	 * 		Параметры запроса:
-	 * 			url: адрес запроса;
-	 * 			method: метод запроса GET или POST;
-	 * 			async: асинхронно или синхронно;
-	 * 			data: данные передоваемые в запросе (строка или объект).
-	 * 		Параметры, прикреплённые к событиям:
-	 * 			success: ответ от сервера успешно получен вместе с кодом html;
-	 * 			beforeSend: функция срабатывает перед AJAX-запросом;
-	 * 			complete: срабатывает по окончанию запроса;
-	 * 			progress: отслеживает загрузку;
-	 * 			errorConnect: срабатывает, если произошла ошибка соединения;
-	 * 			error: сработывает, если произошла ошибка запроса.
-	 */
+/**
+ * XMLHttpRequest-запрос.
+ * 
+ * @param {object} param
+ *     Параметры запроса:
+ *         url: адрес запроса;
+ * 	   method: метод запроса GET или POST;
+ * 	   async: асинхронно или синхронно;
+ * 	   data: данные передоваемые в запросе (строка или объект).
+ *     Параметры, прикреплённые к событиям:
+ * 	   success: ответ от сервера успешно получен вместе с кодом html;
+ * 	   beforeSend: функция срабатывает перед AJAX-запросом;
+ * 	   complete: срабатывает по окончанию запроса;
+ * 	   progress: отслеживает загрузку;
+ * 	   errorConnect: срабатывает, если произошла ошибка соединения;
+ * 	   error: сработывает, если произошла ошибка запроса.
+ */
 	connect( params ) {
 		if ( "url" in params ) {
 			let p = this.DEFAULT_PARAMS;
